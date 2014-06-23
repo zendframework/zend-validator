998a9aa8771865d671ec04d0be56d344c3999577
Marco Pivetta <ocramius@gmail.com>
2014-06-23 19:54:06 +0000

    Importing state as of zendframework/zf2@2a9c97db0b21336d85937caaec2ecda3de6dc474 (1403553246)
    Automatic import via rsync

--- Db/AbstractDb.php
+++ Db/AbstractDb.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -11,6 +11,7 @@ namespace Zend\Validator\Db;
 
 use Traversable;
 use Zend\Db\Adapter\Adapter as DbAdapter;
+use Zend\Db\Adapter\Driver\DriverInterface as DbDriverInterface;
 use Zend\Db\Sql\Select;
 use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\TableIdentifier;
@@ -189,7 +190,6 @@ abstract class AbstractDb extends AbstractValidator
     public function setExclude($exclude)
     {
         $this->exclude = $exclude;
-        $this->select  = null;
         return $this;
     }
 
@@ -211,8 +211,7 @@ abstract class AbstractDb extends AbstractValidator
      */
     public function setField($field)
     {
-        $this->field  = (string) $field;
-        $this->select = null;
+        $this->field = (string) $field;
         return $this;
     }
 
@@ -234,8 +233,7 @@ abstract class AbstractDb extends AbstractValidator
      */
     public function setTable($table)
     {
-        $this->table  = (string) $table;
-        $this->select = null;
+        $this->table = (string) $table;
         return $this;
     }
 
@@ -258,7 +256,6 @@ abstract class AbstractDb extends AbstractValidator
     public function setSchema($schema)
     {
         $this->schema = $schema;
-        $this->select = null;
         return $this;
     }
 
