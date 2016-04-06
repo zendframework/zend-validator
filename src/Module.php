2e58d8df856068c42e17286043ce8b8be70bdf11
Matthew Weier O'Phinney <matthew@zend.com>
2016-04-06 10:22:03 -0500

    Expose as a generic config provider and ZF component
    Adds:
    
    - `ConfigProvider`, which maps the service `ValidatorManager` to the new
      `ValidatorPluginManagerFactory`.
    - `Module`, which maps the service `ValidatorManager` to the new
      `ValidatorPluginManagerFactory`, and adds zend-modulemanager
      `ServiceListener` specifications for providing validator configuration.

---
+++ src/Module.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-validator for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Validator;
+
+class Module
+{
+    /**
+     * Return default zend-validator configuration for zend-mvc applications.
+     */
+    public function getConfig()
+    {
+        $provider = new ConfigProvider();
+
+        return [
+            'service_manager' => $provider->getDependencyConfig(),
+        ];
+    }
+
+    /**
+     * Register a specification for the ValidatorManager with the ServiceListener.
+     *
+     * @param \Zend\ModuleManager\ModuleEvent
+     * @return void
+     */
+    public function init($event)
+    {
+        $container = $event->getParam('ServiceManager');
+        $serviceListener = $container->get('ServiceListener');
+
+        $serviceListener->addServiceManager(
+            'ValidatorManager',
+            'validators',
+            'Zend\ModuleManager\Feature\ValidatorProviderInterface',
+            'getValidatorConfig'
+        );
+    }
+}
