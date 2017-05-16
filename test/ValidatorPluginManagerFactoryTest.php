cd6b79ce97f8dd084b9baa574b8726f909264830
Matthew Weier O'Phinney <matthew@zend.com>
2017-05-16 15:32:31 -0500

    Ensure `validators` config is honored in non-zend-mvc contexts
    Per https://discourse.zendframework.com/t/validatormanager-not-calling-custom-validator-factory/109/5?u=matthew
    the `validators` config key is not honored currently unless the
    application is within a zend-mvc context. This is due to the fact that
    `Zend\Validator\Module` wires configuration for the
    `Zend\ModuleManager\Listener\ServiceListener` in order to push merged
    service configuration into the plugin during bootstrap; no similar logic
    is available when not in a zend-mvc context, however.
    
    This patch fixes that situation by modifying the
    `ValidatorPluginManagerFactory` to do the following:
    
    - If a `ServiceListener` service exists, it returns the plugin manager
      immediately (old behavior).
    - Otherwise, it checks for the `config` service, and, if found, a
      `validators` key with an array value. When found, it feeds that value
      to a `Zend\ServiceManager\Config` instance and uses that to configure
      the plugin manager before returning it.

--- test/ValidatorPluginManagerFactoryTest.php
+++ test/ValidatorPluginManagerFactoryTest.php
@@ -9,6 +9,7 @@ namespace ZendTest\Validator;
 
 use Interop\Container\ContainerInterface;
 use PHPUnit\Framework\TestCase;
+use Zend\Validator\Digits;
 use Zend\Validator\ValidatorInterface;
 use Zend\Validator\ValidatorPluginManager;
 use Zend\Validator\ValidatorPluginManagerFactory;
@@ -69,5 +70,106 @@ class ValidatorPluginManagerFactoryTest extends TestCase
 
         $validators = $factory->createService($container->reveal());
         $this->assertSame($validator, $validators->get('test'));
+    }
+
+    public function testConfiguresValidatorServicesWhenFound()
+    {
+        $validator = $this->prophesize(ValidatorInterface::class)->reveal();
+        $config = [
+            'validators' => [
+                'aliases' => [
+                    'test' => Digits::class,
+                ],
+                'factories' => [
+                    'test-too' => function ($container) use ($validator) {
+                        return $validator;
+                    },
+                ],
+            ],
+        ];
+
+        $container = $this->prophesize(ServiceLocatorInterface::class);
+        $container->willImplement(ContainerInterface::class);
+
+        $container->has('ServiceListener')->willReturn(false);
+        $container->has('config')->willReturn(true);
+        $container->get('config')->willReturn($config);
+        $container->has('MvcTranslator')->willReturn(false); // necessary due to default initializers
+
+        $factory = new ValidatorPluginManagerFactory();
+        $validators = $factory($container->reveal(), 'FormElementManager');
+
+        $this->assertInstanceOf(ValidatorPluginManager::class, $validators);
+        $this->assertTrue($validators->has('test'));
+        $this->assertInstanceOf(Digits::class, $validators->get('test'));
+        $this->assertTrue($validators->has('test-too'));
+        $this->assertSame($validator, $validators->get('test-too'));
+    }
+
+    public function testDoesNotConfigureValidatorServicesWhenServiceListenerPresent()
+    {
+        $validator = $this->prophesize(ValidatorInterface::class)->reveal();
+        $config = [
+            'validators' => [
+                'aliases' => [
+                    'test' => Digits::class,
+                ],
+                'factories' => [
+                    'test-too' => function ($container) use ($validator) {
+                        return $validator;
+                    },
+                ],
+            ],
+        ];
+
+        $container = $this->prophesize(ServiceLocatorInterface::class);
+        $container->willImplement(ContainerInterface::class);
+
+        $container->has('ServiceListener')->willReturn(true);
+        $container->has('config')->shouldNotBeCalled();
+        $container->get('config')->shouldNotBeCalled();
+        $container->has('MvcTranslator')->willReturn(false); // necessary due to default initializers
+
+        $factory = new ValidatorPluginManagerFactory();
+        $validators = $factory($container->reveal(), 'FormElementManager');
+
+        $this->assertInstanceOf(ValidatorPluginManager::class, $validators);
+        $this->assertFalse($validators->has('test'));
+        $this->assertFalse($validators->has('test-too'));
+    }
+
+    public function testDoesNotConfigureValidatorServicesWhenConfigServiceNotPresent()
+    {
+        $validator = $this->prophesize(ValidatorInterface::class)->reveal();
+        $container = $this->prophesize(ServiceLocatorInterface::class);
+        $container->willImplement(ContainerInterface::class);
+
+        $container->has('ServiceListener')->willReturn(false);
+        $container->has('config')->willReturn(false);
+        $container->get('config')->shouldNotBeCalled();
+        $container->has('MvcTranslator')->willReturn(false); // necessary due to default initializers
+
+        $factory = new ValidatorPluginManagerFactory();
+        $validators = $factory($container->reveal(), 'FormElementManager');
+
+        $this->assertInstanceOf(ValidatorPluginManager::class, $validators);
+    }
+
+    public function testDoesNotConfigureValidatorServicesWhenConfigServiceDoesNotContainValidatorsConfig()
+    {
+        $validator = $this->prophesize(ValidatorInterface::class)->reveal();
+        $container = $this->prophesize(ServiceLocatorInterface::class);
+        $container->willImplement(ContainerInterface::class);
+
+        $container->has('ServiceListener')->willReturn(false);
+        $container->has('config')->willReturn(true);
+        $container->get('config')->willReturn(['foo' => 'bar']);
+        $container->has('MvcTranslator')->willReturn(false); // necessary due to default initializers
+
+        $factory = new ValidatorPluginManagerFactory();
+        $validators = $factory($container->reveal(), 'FormElementManager');
+
+        $this->assertInstanceOf(ValidatorPluginManager::class, $validators);
+        $this->assertFalse($validators->has('foo'));
     }
 }
