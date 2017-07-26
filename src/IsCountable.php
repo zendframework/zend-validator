74b4711166ca96825715558067aecd36992b9aee
Matthew Weier O'Phinney <matthew@zend.com>
2017-07-26 10:26:22 -0500

    Adds license docblocks to new class files

--- src/IsCountable.php
+++ src/IsCountable.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @see       https://github.com/zendframework/zend-validator for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-validator/blob/master/LICENSE.md New BSD License
+ */
 
 namespace Zend\Validator;
 
